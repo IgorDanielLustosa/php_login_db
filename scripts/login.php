26c6b06330e624c770c6731439218e79a4baefa9
Igor Daniel A. Lustosa Rosa <105529100+IgorDanielLustosa@users.noreply.github.com>
2024-08-06 19:21:43 -0300

    Adicionei logo

--- scripts/login.php
+++ scripts/login.php
@@ -10,7 +10,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
         body {
-            background-image: url('../img/img02.JPG'); /* Caminho para a sua imagem */
+            background-image: url('../img/img01.JPEG'); /* Caminho para a sua imagem */
             background-size: cover; /* Para ajustar a imagem ao tamanho da tela */
             background-position: center; /* Centraliza a imagem */
             background-repeat: no-repeat; /* Evita repetição da imagem */
@@ -21,12 +21,17 @@
             align-items: center; /* Para centralizar o conteúdo verticalmente */
         }
         .card {
-            background: rgba(255, 255, 255, 0.8); /* Transparência no card */
+            background: rgba(255, 255, 255, 0.6); /* Transparência no card */
             border-radius: 10px;
             padding: 20px;
             width: 100%; /* Faz o card ocupar toda a largura disponível dentro do col */
-            max-width: 400px; /* Define uma largura máxima para o card */            border-radius: 10px;
-            padding: 20px;
+            max-width: 400px; /* Define uma largura máxima para o card */
+        }
+        .logo {
+            display: block;
+            margin: 0 auto 20px auto; /* Centraliza a imagem e adiciona espaço abaixo */
+            max-width: 100%; /* Garante que a imagem não ultrapasse a largura do card */
+            height: auto; /* Mantém a proporção da imagem */
         }
     </style>
 </head>
@@ -40,6 +45,7 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-8 col-lg-6">
                 <div class="card p-5">
+                    <img src="../img/LUSTOSA.png" alt="Logo" class="logo" style="max-width: 100px;"> <!-- Imagem de logo -->
                     <h3 class="text-center">Login</h3>
                     <hr>
                     <form action="?rota=login_submit" method="post">
